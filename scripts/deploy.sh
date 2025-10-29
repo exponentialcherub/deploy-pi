4595556b50610f2e0c0c6fd6a9c8a39a11167c28
Liam Gibbs <44228844+exponentialcherub@users.noreply.github.com>
2025-10-29 17:54:21 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,6 +37,5 @@ git reset --hard "origin/$BRANCH"
 echo "Starting new screen: $SCREEN_NAME"
 echo "Running command $NODE_CMD"
 screen -dmS "$SCREEN_NAME" bash -c "$NODE_CMD"
-bash -c "$NODE_CMD"
 
 echo "? '$APP_NAME' deployed successfully."
